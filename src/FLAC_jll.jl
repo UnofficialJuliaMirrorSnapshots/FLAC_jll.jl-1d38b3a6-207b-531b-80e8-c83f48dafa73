1526e9b44517156eeea3412754937ab7df50e72c
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-10-30 11:36:31 +0000

    Snapshot of branch master taken on 2019-10-30T07:36:30.673-04:00 from "https://github.com/UnofficialJuliaMirror/FLAC_jll.jl-1d38b3a6-207b-531b-80e8-c83f48dafa73"

---
+++ src/FLAC_jll.jl
@@ -0,0 +1,31 @@
+module FLAC_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# We put these inter-JLL-package API values here so that they are always defined, even if there
+# is no underlying wrapper held within this JLL package.
+const PATH_list = String[]
+const LIBPATH_list = String[]
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("1d38b3a6-207b-531b-80e8-c83f48dafa73"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "FLAC", artifacts_toml) for e in artifacts["FLAC"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load FLAC; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module FLAC_jll
